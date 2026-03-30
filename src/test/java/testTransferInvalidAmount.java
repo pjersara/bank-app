283676d8960ecfad2ed84ea15d370f3447926bb3
pjersara <pjersara@gmail.com>
2026-03-30 14:20:13 +0200

    Implement test for invalid transfer amount
    Add test for transferring invalid negative amount.

---
+++ src/test/java/testTransferInvalidAmount.java
@@ -0,0 +1,14 @@
+@Test
+void testTransferInvalidAmount() {
+    AccountRepository repo = new AccountRepository();
+    AccountService accService = new AccountService(repo);
+    TransactionService tx = new TransactionService(accService);
+
+    repo.save(new Account("A1", 500));
+    repo.save(new Account("A2", 100));
+
+    tx.transfer("A1", "A2", -100);
+
+    assertEquals(500, repo.findById("A1").balance);
+    assertEquals(100, repo.findById("A2").balance);
+}
