dfaa7c7fd68da86a1d4f0724fbdd2edc29a565ab
pjersara <pjersara@gmail.com>
2026-03-30 14:42:22 +0200

    Update testTransferInvalidAmount to JUnit 5 format
    Refactor test for transferring invalid amount to use JUnit 5.

--- src/test/java/testTransferInvalidAmount.java
+++ src/test/java/testTransferInvalidAmount.java
@@ -1,14 +1,24 @@
-@Test
-void testTransferInvalidAmount() {
-    AccountRepository repo = new AccountRepository();
-    AccountService accService = new AccountService(repo);
-    TransactionService tx = new TransactionService(accService);
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-    repo.save(new Account("A1", 500));
-    repo.save(new Account("A2", 100));
+class testTransferZeroAmount {
 
-    tx.transfer("A1", "A2", -100);
+    @Test
+    void testTransferInvalidAmount() {
+        AccountRepository repo = new AccountRepository();
+        AccountService accService = new AccountService(repo);
+        TransactionService tx = new TransactionService(accService);
 
-    assertEquals(500, repo.findById("A1").balance);
-    assertEquals(100, repo.findById("A2").balance);
+        repo.save(new Account("A1", 500));
+        repo.save(new Account("A2", 100));
+
+        tx.transfer("A1", "A2", -100);
+
+        assertEquals(500, repo.findById("A1").balance);
+        assertEquals(100, repo.findById("A2").balance);
+    }
 }
+
+
+
+
