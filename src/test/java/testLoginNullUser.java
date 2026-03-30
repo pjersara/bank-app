889d30699492a982a854f4288dc923fb300244ff
pjersara <pjersara@gmail.com>
2026-03-30 14:40:45 +0200

    Refactor testLoginNullUser method indentation

--- src/test/java/testLoginNullUser.java
+++ src/test/java/testLoginNullUser.java
@@ -1,14 +1,11 @@
-
-
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class testDepositNegativeAmount {
 
     @Test
-void testLoginNullUser() {
-    AuthService auth = new AuthService();
-
-    assertFalse(auth.login(null, "pass"));
-}
+    void testLoginNullUser() {
+        AuthService auth = new AuthService();
+        assertFalse(auth.login(null, "pass"));
+    }
 }
