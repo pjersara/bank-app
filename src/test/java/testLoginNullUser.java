1fbd85ffcd818eb06a485e99fae71aa11936bda0
pjersara <pjersara@gmail.com>
2026-03-30 14:39:46 +0200

    Add test for login with null user

--- src/test/java/testLoginNullUser.java
+++ src/test/java/testLoginNullUser.java
@@ -1,6 +1,14 @@
-@Test
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class testDepositNegativeAmount {
+
+    @Test
 void testLoginNullUser() {
     AuthService auth = new AuthService();
 
     assertFalse(auth.login(null, "pass"));
+}
 }
