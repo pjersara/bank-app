afbd42d815955484d8b34a22164281e68d6504b5
pjersara <pjersara@gmail.com>
2026-03-30 14:38:34 +0200

    Refactor test for depositing negative amounts

--- src/test/java/testDepositNegativeAmount.java
+++ src/test/java/testDepositNegativeAmount.java
@@ -1,11 +1,17 @@
-@Test
-void testDepositNegativeAmount() {
-    AccountRepository repo = new AccountRepository();
-    AccountService service = new AccountService(repo);
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-    repo.save(new Account("A1", 100));
+class testDepositNegativeAmount {
 
-    service.deposit("A1", -50);
+    @Test
+    void testDepositNegativeAmount() {
+        AccountRepository repo = new AccountRepository();
+        AccountService service = new AccountService(repo);
 
-    assertEquals(50, repo.findById("A1").balance); // pokazuje bug
+        repo.save(new Account("A1", 100));
+
+        service.deposit("A1", -50);
+
+        assertEquals(50, repo.findById("A1").balance); // pokazuje bug
+    }
 }
