ede04969b3de282d10011e7c6119824ca896ae1e
pjersara <pjersara@gmail.com>
2026-03-31 09:08:42 +0200

    Create CoverageBoosterTest.java

---
+++ src/test/java/CoverageBoosterTest.java
@@ -0,0 +1,61 @@
+import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CoverageBoosterTest {
+
+    @Test
+    void testMainRunsWithoutException() {
+        assertDoesNotThrow(() -> Main.main(new String[]{}));
+    }
+
+    @Test
+    void testRepositorySaveAndFindAll() {
+        AccountRepository repo = new AccountRepository();
+
+        repo.save(new Account("A1", 100));
+        repo.save(new Account("A2", 200));
+
+        assertEquals(2, repo.findAll().size());
+        assertNotNull(repo.findById("A1"));
+        assertEquals(100, repo.findById("A1").balance);
+    }
+
+    @Test
+    void testControllerTransferWrongPassword() {
+        AccountRepository repo = new AccountRepository();
+        repo.save(new Account("A1", 1000));
+        repo.save(new Account("A2", 500));
+
+        AccountService acc = new AccountService(repo);
+        TransactionService tx = new TransactionService(acc);
+        LoanService loan = new LoanService(acc);
+        AuthService auth = new AuthService();
+
+        BankController controller = new BankController(auth, tx, loan);
+
+        User user = new User("john", "secret", "user");
+
+        assertThrows(SecurityException.class,
+                () -> controller.transfer(user, "wrong", "A1", "A2", 100));
+    }
+
+    @Test
+    void testSingleBatchTransferBranch() {
+        AccountRepository repo = new AccountRepository();
+        repo.save(new Account("A1", 500));
+        repo.save(new Account("A2", 100));
+
+        AccountService acc = new AccountService(repo);
+        TransactionService tx = new TransactionService(acc);
+
+        tx.batchTransfer(
+                Arrays.asList("A1"),
+                Arrays.asList("A2"),
+                Arrays.asList(50.0)
+        );
+
+        assertEquals(450, repo.findById("A1").balance);
+        assertEquals(150, repo.findById("A2").balance);
+    }
+}
